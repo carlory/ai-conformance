e05fd4dfa03b32389f49307c50b52c9cad2f3655
carlory <baofa.fan@daocloud.io>
2025-09-16 09:57:18 +0800

    add tests
    Signed-off-by: carlory <baofa.fan@daocloud.io>

--- ai-conformance/pkg/observability_test.go
+++ ai-conformance/pkg/observability_test.go
@@ -2,8 +2,19 @@ package pkg
 
 import (
 	"context"
+	"fmt"
+	"io/fs"
+	"os"
 	"testing"
+	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/kubectl/pkg/util/podutils"
+	"sigs.k8s.io/e2e-framework/klient/decoder"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -15,19 +26,44 @@ import (
 // those are standardized.
 //
 // See https://docs.google.com/document/d/1hXoSdh9FEs13Yde8DivCYjjXyxa7j4J8erjZPEGWuzc/edit?tab=t.0
-func TestAcceleratorMmetrics(t *testing.T) {
-	description := "For supported accelerator types, expose fine-grained performance metrics via a standardized, " +
-		"machine-readable format metrics endpoint. This must include a core set of metrics for per-accelerator utilization and memory usage." +
-		"Additionally, other relevant metrics such as temperature, power draw, and interconnect bandwidth should be exposed if the underlying " +
-		"hardware or virtualization layer makes them available. The list of metrics should align with emerging standards, such as OpenTelemetry " +
-		"metrics, to ensure interoperability."
+func TestAcceleratorMetrics(t *testing.T) {
+	description := "For supported accelerator types, the platform must allow for the installation and successful operation of at least " +
+		"one accelerator metrics solution that exposes fine-grained performance metrics via a standardized, machine-readable metrics endpoint. " +
+		"This must include a core set of metrics for per-accelerator utilization and memory usage. Additionally, other relevant metrics such as " +
+		"temperature, power draw, and interconnect bandwidth should be exposed if the underlying hardware or virtualization layer makes them available." +
+		"The list of metrics should align with emerging standards, such as OpenTelemetry metrics, to ensure interoperability. " +
+		"The platform may provide a managed solution, but this is not required for conformance."
 
 	f := features.New("accelerator_metrics").
 		WithLabel("type", "observability").
 		WithLabel("id", "accelerator_metrics").
-		WithLabel("description", description).
 		WithLabel("level", "MUST").
-		Assess(description, func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		AssessWithDescription("Verify that the metrics are collected from the GPU node", description, func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			jobName := "nvidia-dcgm-exporter"
+
+			// TODO: use a better way to retrieve the metrics.
+			t.Logf("Waiting for the metrics to be collected")
+			time.Sleep(30 * time.Second)
+
+			queryString := fmt.Sprintf(`count by (__name__) ({job="%s"})`, jobName)
+
+			raw, err := QueryPrometheus(PrometheusQueryParams{
+				Config:                cfg,
+				PrometheusmNamespace:  "monitoring",
+				PrometheusServiceName: "kube-prometheus-stack-prometheus",
+				Query:                 queryString,
+			})
+			if err != nil {
+				t.Errorf("error when getting prometheus query: %v", err)
+				return ctx
+			}
+			t.Logf("prometheus query result: \n%s", string(raw))
+
+			if raw == "" {
+				t.Errorf("prometheus query result is empty")
+				return ctx
+			}
+			// FIXME: I don't know which kind of metrics should be checked.
 			return ctx
 		})
 
@@ -41,14 +77,86 @@ func TestAcceleratorMmetrics(t *testing.T) {
 // key metrics from the AI application have been collected.
 //
 // See https://docs.google.com/document/d/1hXoSdh9FEs13Yde8DivCYjjXyxa7j4J8erjZPEGWuzc/edit?tab=t.0
-func TestAcceleratorMetrics(t *testing.T) {
+func TestAIServiceMetrics(t *testing.T) {
 	description := "Provide a monitoring system capable of discovering and collecting metrics from workloads that expose them in a standard format " +
 		"(e.g. Prometheus exposition format). This ensures easy integration for collecting key metrics from common AI frameworks and servers."
 
+	type PrometheusJobNameContextKey struct{}
 	f := features.New("ai_service_metrics").
 		WithLabel("type", "observability").
 		WithLabel("id", "ai_service_metrics").
-		Assess(description, func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			SkipIfGroupVersionUnavaliable(t, cfg, "monitoring.coreos.com/v1")
+
+			var prometheusJobName string
+			createFn := func(ctx context.Context, obj k8s.Object) error {
+				t.Logf("Creating %s %s", obj.GetObjectKind().GroupVersionKind().Kind, obj.GetName())
+				if obj.GetObjectKind().GroupVersionKind().Kind == "ServiceMonitor" {
+					prometheusJobName = obj.GetName()
+				}
+				return cfg.Client().Resources().Create(ctx, obj)
+			}
+
+			err := decoder.DecodeEachFile(ctx, fs.FS(os.DirFS("testdata")), "ai_service_metrics/*.yaml", createFn, decoder.MutateNamespace(cfg.Namespace()))
+			if err != nil {
+				t.Error(err)
+				return ctx
+			}
+
+			// Wait for the pods to be created.
+			time.Sleep(5 * time.Second)
+
+			pods := &corev1.PodList{}
+			err = cfg.Client().Resources(cfg.Namespace()).List(ctx, pods)
+			if err != nil {
+				t.Errorf("error when getting Pods in the namespace: %s: %v", cfg.Namespace(), err)
+				return ctx
+			}
+
+			t.Logf("Waiting for %d Pods to be ready", len(pods.Items))
+			err = wait.For(conditions.New(cfg.Client().Resources()).ResourcesMatch(pods, func(obj k8s.Object) bool {
+				return podutils.IsPodReady(obj.(*corev1.Pod))
+			}))
+			if err != nil {
+				t.Errorf("error when checking all Pods ready in the namespace %s: %v", cfg.Namespace(), err)
+				return ctx
+			}
+
+			return context.WithValue(ctx, PrometheusJobNameContextKey{}, prometheusJobName)
+		}).
+		AssessWithDescription("Verify that the metrics are collected from the AI workload", description, func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			jobName := ctx.Value(PrometheusJobNameContextKey{}).(string)
+
+			// TODO: use a better way to retrieve the metrics.
+			t.Logf("Waiting for the metrics to be collected")
+			time.Sleep(time.Minute)
+
+			queryString := fmt.Sprintf(`count by (__name__) ({job="%s", namespace="%s"})`, jobName, cfg.Namespace())
+
+			raw, err := QueryPrometheus(PrometheusQueryParams{
+				Config:                cfg,
+				PrometheusmNamespace:  "monitoring",
+				PrometheusServiceName: "kube-prometheus-stack-prometheus",
+				Query:                 queryString,
+			})
+			if err != nil {
+				t.Errorf("error when getting prometheus query: %v", err)
+				return ctx
+			}
+			t.Logf("prometheus query result: \n%s", string(raw))
+
+			if raw == "" {
+				t.Errorf("prometheus query result is empty")
+				return ctx
+			}
+			// FIXME: I don't know which kind of metrics should be checked.
+			return ctx
+		}).
+		Teardown(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			err := decoder.DeleteWithManifestDir(ctx, cfg.Client().Resources(), "testdata", "ai_service_metrics/*.yaml", []resources.DeleteOption{}, decoder.MutateNamespace(cfg.Namespace()))
+			if err != nil {
+				t.Errorf("error when deleting the resources: %v", err)
+			}
 			return ctx
 		})
 
--- ai-conformance/pkg/security_test.go
+++ ai-conformance/pkg/security_test.go
@@ -1,9 +1,20 @@
 package pkg
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"io/fs"
+	"os"
+	"strings"
 	"testing"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/kubectl/pkg/util/podutils"
+	"sigs.k8s.io/e2e-framework/klient/decoder"
+	"sigs.k8s.io/e2e-framework/klient/k8s"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -22,9 +33,119 @@ func TestSecureAcceleratorAccess(t *testing.T) {
 	f := features.New("secure_accelerator_access").
 		WithLabel("type", "security").
 		WithLabel("id", "secure_accelerator_access").
-		WithLabel("description", description).
 		WithLabel("level", "MUST").
-		Assess(description, func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		AssessWithDescription("should fail to probe for accelerator devices when the pod is not allocated an accelerator resource", description, func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			targetPod := &corev1.Pod{}
+			client := cfg.Client()
+			createPodFn := func(ctx context.Context, obj k8s.Object) error {
+				t.Logf("Creating pod with generate name %s", obj.GetGenerateName())
+				pod, ok := obj.(*corev1.Pod)
+				if !ok {
+					return fmt.Errorf("expected a pod, got %T", obj)
+				}
+
+				t.Logf("Setting pod's namespace to %s", cfg.Namespace())
+				pod.SetNamespace(cfg.Namespace())
+				t.Logf("Remove accelerators resources from the requrests and limits of the pod's containers")
+				podutils.VisitContainers(&pod.Spec, podutils.AllContainers, func(container *corev1.Container, containerType podutils.ContainerType) bool {
+					for rName := range container.Resources.Limits {
+						if rName == corev1.ResourceCPU || rName == corev1.ResourceMemory {
+							continue
+						}
+						t.Logf("Removing %s from the container's resources limits", rName)
+						delete(container.Resources.Limits, rName)
+					}
+					return true
+				})
+				err := client.Resources().Create(ctx, obj)
+				if err != nil {
+					return err
+				}
+				targetPod = pod
+				t.Logf("Pod %s created", pod.GetName())
+				return nil
+			}
+			err := decoder.DecodeEachFile(ctx, fs.FS(os.DirFS("testdata")), "secure_accelerator_access/pod-template.yaml", createPodFn, decoder.MutateNamespace(cfg.Namespace()))
+			if err != nil {
+				t.Error(err)
+				return ctx
+			}
+			defer func() {
+				t.Logf("Deleting pod %s", targetPod.GetName())
+				client.Resources().Delete(ctx, targetPod)
+			}()
+
+			t.Logf("Waiting for pod %s to be ready", targetPod.GetName())
+			err = wait.For(conditions.New(client.Resources()).PodReady(targetPod))
+			if err != nil {
+				t.Errorf("error when waiting for pod %s to be running: %v", targetPod.GetName(), err)
+				return ctx
+			}
+
+			cmd := []string{"nvidia-smi"}
+			t.Logf("Executing command %q in pod %s", cmd, targetPod.GetName())
+			stdout := &bytes.Buffer{}
+			stderr := &bytes.Buffer{}
+			err = client.Resources().ExecInPod(ctx, cfg.Namespace(), targetPod.GetName(), targetPod.Spec.Containers[0].Name, cmd, stdout, stderr)
+			if err != nil || stderr.Len() > 0 {
+				return ctx
+			}
+			t.Errorf("unexpected successful result, stdout: %s", stdout.String())
+			return ctx
+		}).
+		AssessWithDescription("should be denied to access the accelerator devices of other pods", description, func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			client := cfg.Client()
+
+			// FIXME: nvidia-smi --query-gpu=index,name --format=csv,noheader
+			// It is not supported by fake-gpu-operator.
+			cmd := []string{"nvidia-smi"}
+
+			pods := []*corev1.Pod{}
+			createPodFn := func(ctx context.Context, obj k8s.Object) error {
+				t.Logf("Creating pod with generate name %s", obj.GetGenerateName())
+				pod, ok := obj.(*corev1.Pod)
+				if !ok {
+					return fmt.Errorf("expected a pod, got %T", obj)
+				}
+				err := client.Resources().Create(ctx, obj)
+				if err != nil {
+					return err
+				}
+				pods = append(pods, pod)
+				return nil
+			}
+
+			t.Logf("Creating two pods")
+			buffers := []*bytes.Buffer{}
+			for i := range 2 {
+				err := decoder.DecodeEachFile(ctx, fs.FS(os.DirFS("testdata")), "secure_accelerator_access/pod-template.yaml", createPodFn, decoder.MutateNamespace(cfg.Namespace()))
+				if err != nil {
+					t.Error(err)
+					return ctx
+				}
+
+				t.Logf("Waiting for pod %s to be ready", pods[i].GetName())
+				err = wait.For(conditions.New(client.Resources()).PodReady(pods[i]))
+				if err != nil {
+					t.Errorf("error when waiting for pod %s to be running: %v", pods[i].GetName(), err)
+					return ctx
+				}
+				stdout := &bytes.Buffer{}
+				stderr := &bytes.Buffer{}
+				buffers = append(buffers, stdout)
+				t.Logf("Executing command %q in pod %s", cmd, pods[i].GetName())
+				err = client.Resources().ExecInPod(ctx, cfg.Namespace(), pods[i].GetName(), pods[i].Spec.Containers[0].Name, cmd, stdout, stderr)
+				if err != nil || stderr.Len() > 0 {
+					t.Errorf("error when executing command in pod %s: %v", pods[i].GetName(), err)
+					return ctx
+				}
+				t.Logf("Got output from pod %s:\n %s", pods[i].GetName(), stdout.String())
+			}
+
+			if strings.EqualFold(buffers[0].String(), buffers[1].String()) {
+				t.Error("the output from the two pods is the same which means they can access each other's accelerator devices")
+				return ctx
+			}
 			return ctx
 		})
 
